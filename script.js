867be46dc12ab9a7dcf5252e6373a0174bc3139f
ISSAO TORIBIO TAKEUCHI <issaottakeuchi@gmail.com>
2022-03-18 21:09:09 -0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,33 @@
+
+function addNewCard(){
+    let exampleInputTitle = document.getElementById('exampleInputTitle').value;
+    let exampleInputDescription = document.getElementById('exampleInputDescription').value;
+    let exampleInputURL = document.getElementById('exampleInputURL').value;
+
+    let sectionToInclude = document.getElementById('cards')
+    let newCardToInsert = document.createElement('div');
+    newCardToInsert.innerHTML = 
+    `<div class="containerOfCards">
+      <div class="row">
+        <div class="col-sm-4">
+          <div class="card">
+            <img src="${exampleInputURL}" id="imageURL" class="card-img-top" alt="">
+            <div class="card-body">
+              <h5 class="card-title" id="cardTitle">${exampleInputTitle}</h5>
+              <p class="card-text" id="cardDescription">${exampleInputDescription}</p>
+              <a href="#" class="btn btn-primary">Detalhes</a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>`;
+    sectionToInclude.appendChild(newCardToInsert); 
+}
+
+
+document.getElementById('sendNewCard').addEventListener('click', (event) => {
+    event.preventDefault()
+    addNewCard()
+})
+
+
